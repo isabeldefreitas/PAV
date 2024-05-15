d6372ef1bdcca4e47ca5cde09d3fa7a8571ae3d9
Isabel de Freitas <127349333+isabeldefreitas@users.noreply.github.com>
2024-05-15 00:25:57 -0300

    atualizações

---
+++ Scripts/NOVOScript.sql
@@ -0,0 +1,200 @@
+
+use `pavbanconovo`;
+
+
+-- Tabela de Produtos
+CREATE TABLE Produto (
+    id_produto INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(100),
+    quantidade_estoque VARCHAR(10),
+    preco DECIMAL(10, 2),
+    unidade VARCHAR(20),
+    id_fornecedor INT,
+    id_tipo INT,
+    FOREIGN KEY (id_fornecedor) REFERENCES Fornecedor(id_fornecedor),
+    FOREIGN KEY (id_tipo) REFERENCES Tipo(id_tipo)
+);
+
+-- Tabela de Clientes
+CREATE TABLE Cliente (
+    id_cliente INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(100),
+    cpf_cnpj VARCHAR(20),
+    logradouro VARCHAR(100),
+    numero VARCHAR(10),
+    complemento VARCHAR(100),
+    bairro VARCHAR(50),
+    cidade VARCHAR(50),
+    estado VARCHAR(50),
+    cep VARCHAR(20),
+    telefone VARCHAR(20),
+    email VARCHAR(100)
+);
+
+
+-- Tabela de Fornecedores
+CREATE TABLE Fornecedor (
+    id_fornecedor INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(100),
+    cpf_cnpj VARCHAR(20),
+    logradouro VARCHAR(100),
+    numero VARCHAR(10),
+    complemento VARCHAR(100),
+    bairro VARCHAR(50),
+    cidade VARCHAR(50),
+    estado VARCHAR(50),
+    cep VARCHAR(20),
+    telefone VARCHAR(20),
+    email VARCHAR(100)
+);
+
+
+
+-- Tabela de Formas de Pagamento
+CREATE TABLE FormaPagamento (
+    id_forma_pagamento INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(50)
+);
+
+-- Tabela de Vendas
+CREATE TABLE Venda (
+    id_venda INT AUTO_INCREMENT PRIMARY KEY,
+    data DATE,
+    hora TIME,
+    id_cliente INT,
+    id_itemVenda INT,
+    total_venda DECIMAL(10, 2),
+    situacao_venda VARCHAR(20),
+    FOREIGN KEY (id_cliente) REFERENCES Cliente(id_cliente)
+);
+
+
+
+-- Tabela de Itens de Venda
+CREATE TABLE ItemVenda (
+    id_itemVenda INT AUTO_INCREMENT PRIMARY KEY,
+    id_venda INT,
+    numeroItem INT,
+    id_produto INT,
+    quantidade VARCHAR(10),
+    valor_unitario DECIMAL(10, 2),
+    total_item DECIMAL(10, 2),
+    FOREIGN KEY (id_produto) REFERENCES Produto(id_produto),
+    FOREIGN KEY (id_venda) REFERENCES venda(id_venda)
+);
+
+
+
+-- Tabela de Formas de Pagamento de Vendas
+CREATE TABLE FormaPagamentoVenda (
+	id_formapagamentovenda INT AUTO_INCREMENT PRIMARY KEY,
+    id_venda INT,
+    id_forma_pagamento INT,
+    valor DECIMAL(10, 2),
+    FOREIGN KEY (id_venda) REFERENCES Venda(id_venda),
+    FOREIGN KEY (id_forma_pagamento) REFERENCES FormaPagamento(id_forma_pagamento)
+);
+
+
+-- Tabela de Compras
+CREATE TABLE Compra (
+    id_compra INT AUTO_INCREMENT PRIMARY KEY,
+    data DATE,
+    hora TIME,
+    id_fornecedor INT,
+    total_compra DECIMAL(10, 2),
+    situacao_compra VARCHAR(20),
+    FOREIGN KEY (id_fornecedor) REFERENCES Fornecedor(id_fornecedor)
+);
+
+
+-- Tabela de Itens de Compra
+CREATE TABLE ItemCompra (
+    id_itemcompra INT AUTO_INCREMENT PRIMARY KEY,
+    id_compra INT,
+    numeroItem INT,
+    id_produto INT,
+    quantidade VARCHAR(10),
+    valor_unitario DECIMAL(10, 2),
+    total_item DECIMAL(10, 2),
+    FOREIGN KEY (id_compra) REFERENCES Compra(id_compra),
+    FOREIGN KEY (id_produto) REFERENCES Produto(id_produto)
+);
+
+-- Tabela de Contas a Receber
+CREATE TABLE Conta_Receber (
+    id_conta_receber INT AUTO_INCREMENT PRIMARY KEY,
+    descricao VARCHAR(200),
+    id_cliente INT,
+    data_lancamento DATE,
+    data_vencimento DATE,
+    valor DECIMAL(10, 2),
+    recebido BOOLEAN,
+    data_recebimento DATE,
+    valor_recebimento DECIMAL(10, 2),
+    FOREIGN KEY (id_cliente) REFERENCES Cliente(id_cliente)
+);
+
+-- Tabela de Contas a Pagar
+CREATE TABLE Conta_Pagar (
+    id_conta_pagar INT AUTO_INCREMENT PRIMARY KEY,
+    descricao VARCHAR(200),
+    id_fornecedor INT,
+    data_lancamento DATE,
+    data_vencimento DATE,
+    valor DECIMAL(10, 2),
+    pago BOOLEAN,
+    data_pagamento DATE,
+    valor_pagamento DECIMAL(10, 2),
+    FOREIGN KEY (id_fornecedor) REFERENCES Fornecedor(id_fornecedor)
+);
+
+
+-- Tabela de Caixa
+CREATE TABLE Caixa (
+    id_caixa INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(100),
+    saldo DECIMAL(10, 2)
+);
+
+
+
+-- Tabela de Movimento de Caixa
+CREATE TABLE MovimentoCaixa (
+    id_movimento_caixa INT AUTO_INCREMENT PRIMARY KEY,
+    id_caixa INT,
+    id_compra INT,
+    id_venda INT,
+    data_movimento DATE,
+    hora_movimento TIME,
+    descricao VARCHAR(200),
+    tipo_movimento VARCHAR(20),
+    valor DECIMAL(10, 2),
+    FOREIGN KEY (id_caixa) REFERENCES Caixa(id_caixa),
+    FOREIGN KEY (id_compra) REFERENCES Compra(id_compra),
+    FOREIGN KEY (id_venda) REFERENCES Venda(id_venda)
+);
+
+
+
+-- Tabela de Tipos
+CREATE TABLE Tipo (
+    id_tipo INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(100)
+);
+
+
+-- -------------------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
--- Scripts/insercao.sql
+++ Scripts/insercao.sql
@@ -1,3 +1,5 @@
+use `pavbanconovo`;
+
 INSERT INTO Produto (nome, quantidade_estoque, preco, unidade, id_fornecedor, id_tipo) 
 VALUES 
 ('Arroz Integral', '50', '7.99', 'kg', 1, 1),
@@ -9,7 +11,51 @@ VALUES
 ('Calça Jeans', '25', '49.99', 'unidade', 7, 3),
 ('Protetor Solar FPS 50', '35', '29.90', 'unidade', 8, 4),
 ('Açúcar Mascavo', '45', '8.50', 'kg', 9, 1),
-('Secador de Cabelo', '10', '89.99', 'unidade', 10, 2);
+('Secador de Cabelo', '10', '89.99', 'unidade', 10, 2),
+('Macarrão Espaguete', '120', 2.99, 'Kg', 4, 1),
+('Azeite de Oliva Extra Virgem', '40', 15.90, 'Litro', 5, 1),
+('Ovos Brancos', '200', 0.50, 'Unidade', 6, 1),
+('Frango Congelado', '60', 9.99, 'Kg', 7, 1),
+('Batata Inglesa', '70', 2.00, 'Kg', 8, 1),
+('Tomate', '90', 3.50, 'Kg', 9, 1),
+('Banana Prata', '150', 2.99, 'Kg', 10, 1),
+('Notebook Dell Inspiron 15', '10', 3500.00, 'Unidade', 2, 2),
+('Smart TV LG 55 Polegadas', '5', 2500.00, 'Unidade', 3, 2),
+('Fone de Ouvido Bluetooth JBL', '30', 150.00, 'Unidade', 6, 2),
+('Carregador Portátil Xiaomi', '40', 50.00, 'Unidade', 6, 2),
+('Câmera Digital Canon EOS Rebel T7', '8', 1500.00, 'Unidade', 2, 2),
+('Console de Videogame PlayStation 5', '3', 4500.00, 'Unidade', 2, 2),
+('Roteador Wi-Fi TP-Link Archer C6', '15', 200.00, 'Unidade', 6, 2),
+('Impressora Multifuncional Epson EcoTank', '7', 800.00, 'Unidade', 6, 2),
+('Mouse Gamer Logitech G502', '25', 100.00, 'Unidade', 6, 2),
+('Camiseta Básica Preta', '80', 20.00, 'Unidade', 9, 3),
+('Calça Jeans Masculina', '40', 80.00, 'Unidade', 7, 3),
+('Vestido Floral', '30', 60.00, 'Unidade', 9, 3),
+('Tênis Nike Air Max', '25', 200.00, 'Unidade', 9, 3),
+('Blusa de Frio', '50', 70.00, 'Unidade', 9, 3),
+('Bermuda Jeans', '35', 50.00, 'Unidade', 7, 3),
+('Saia Midi Plissada', '20', 45.00, 'Unidade', 9, 3),
+('Camisa Social Masculina', '15', 100.00, 'Unidade', 9, 3),
+('Jaqueta de Couro', '10', 150.00, 'Unidade', 9, 3),
+('Legging Fitness', '60', 30.00, 'Unidade', 7, 3),
+('Shampoo Pantene 3 em 1', '50', 15.00, 'Litro', 8, 4),
+('Protetor Solar Neutrogena FPS 50', '30', 40.00, 'Unidade', 8, 4),
+('Batom Ruby Woo da MAC', '20', 50.00, 'Unidade', 8, 4),
+('Creme Hidratante Nivea Soft', '70', 20.00, 'Unidade', 8, 4);
+
+
+INSERT INTO Fornecedor (nome, cpf_cnpj, logradouro, numero, complemento, bairro, cidade, estado, cep, telefone, email) 
+VALUES 
+('Distribuidora ABC', '012.345.678/0001-01', 'Rua das Indústrias', '200', '', 'Industrial', 'São Paulo', 'SP', '23456-789', '(11) 7777-7777', 'contato@distribuidora.com'),
+('Tecnosmart LTDA', '987.654.321/0001-01', 'Avenida Tecnológica', '300', '', 'Tecnopark', 'Rio de Janeiro', 'RJ', '34567-890', '(21) 6666-6666', 'contato@tecnosmart.com'),
+('VestBem Comércio', '222.333.444/0001-01', 'Rua da Moda', '400', '', 'Fashion', 'Belo Horizonte', 'MG', '45678-901', '(31) 8888-8888', 'contato@vestbem.com'),
+('Beleza e Estilo Ltda', '333.444.555/0001-01', 'Avenida das Belezas', '500', '', 'Beauty', 'Salvador', 'BA', '56789-012', '(71) 9999-9999', 'contato@beleza.com'),
+('Cereais do Brasil', '444.555.666/0001-01', 'Rua dos Cereais', '600', '', 'Grãos', 'Curitiba', 'PR', '67890-123', '(41) 3333-3333', 'contato@cereais.com'),
+('CellTech Solutions', '555.666.777/0001-01', 'Avenida das Tecnologias', '700', '', 'Tecno', 'Recife', 'PE', '78901-234', '(81) 2222-2222', 'contato@celltech.com'),
+('Jeans Wear Ltda', '666.777.888/0001-01', 'Rua do Jeans', '800', '', 'Denim', 'Fortaleza', 'CE', '90123-456', '(85) 5555-5555', 'contato@jeanswear.com'),
+('Sun Beauty Supply', '777.888.999/0001-01', 'Avenida do Sol', '900', '', 'Sun', 'Goiânia', 'GO', '12345-678', '(62) 7777-7777', 'contato@sunbeauty.com'),
+('Fashion Trends', '888.999.000/0001-01', 'Rua da Moda', '1000', '', 'Trend', 'Porto Alegre', 'RS', '23456-789', '(51) 1111-1111', 'contato@fashiontrends.com'),
+('BeautyStyle', '999.000.111/0001-01', 'Avenida da Beleza', '1100', '', 'Style', 'Manaus', 'AM', '34567-890', '(92) 9999-9999', 'contato@beautystyle.com');
 
 
 INSERT INTO Cliente (nome, cpf_cnpj, logradouro, numero, complemento, bairro, cidade, estado, cep, telefone, email) 
@@ -25,20 +71,6 @@ VALUES
 ('Juliana Pereira', '987.654.321-09', 'Rua das Orquídeas', '75', '', 'Orquidário', 'Porto Alegre', 'RS', '09876-543', '(51) 8888-8888', 'juliana@email.com'),
 ('Marcos Santos', '345.678.901-23', 'Avenida das Violetas', '120', 'Casa 3', 'Violeta', 'Manaus', 'AM', '23456-789', '(92) 7777-7777', 'marcos@email.com');
 
-
-
-INSERT INTO Fornecedor (nome, cpf_cnpj, logradouro, numero, complemento, bairro, cidade, estado, cep, telefone, email) 
-VALUES 
-('Distribuidora ABC', '012.345.678/0001-01', 'Rua das Indústrias', '200', '', 'Industrial', 'São Paulo', 'SP', '23456-789', '(11) 7777-7777', 'contato@distribuidora.com'),
-('Tecnosmart LTDA', '987.654.321/0001-01', 'Avenida Tecnológica', '300', '', 'Tecnopark', 'Rio de Janeiro', 'RJ', '34567-890', '(21) 6666-6666', 'contato@tecnosmart.com'),
-('VestBem Comércio', '222.333.444/0001-01', 'Rua da Moda', '400', '', 'Fashion', 'Belo Horizonte', 'MG', '45678-901', '(31) 8888-8888', 'contato@vestbem.com'),
-('Beleza e Estilo Ltda', '333.444.555/0001-01', 'Avenida das Belezas', '500', '', 'Beauty', 'Salvador', 'BA', '56789-012', '(71) 9999-9999', 'contato@beleza.com'),
-('Cereais do Brasil', '444.555.666/0001-01', 'Rua dos Cereais', '600', '', 'Grãos', 'Curitiba', 'PR', '67890-123', '(41) 3333-3333', 'contato@cereais.com'),
-('CellTech Solutions', '555.666.777/0001-01', 'Avenida das Tecnologias', '700', '', 'Tecno', 'Recife', 'PE', '78901-234', '(81) 2222-2222', 'contato@celltech.com'),
-('Jeans Wear Ltda', '666.777.888/0001-01', 'Rua do Jeans', '800', '', 'Denim', 'Fortaleza', 'CE', '90123-456', '(85) 5555-5555', 'contato@jeanswear.com'),
-('Sun Beauty Supply', '777.888.999/0001-01', 'Avenida do Sol', '900', '', 'Sun', 'Goiânia', 'GO', '12345-678', '(62) 7777-7777', 'contato@sunbeauty.com'),
-('Fashion Trends', '888.999.000/0001-01', 'Rua da Moda', '1000', '', 'Trend', 'Porto Alegre', 'RS', '23456-789', '(51) 1111-1111', 'contato@fashiontrends.com'),
-('BeautyStyle', '999.000.111/0001-01', 'Avenida da Beleza', '1100', '', 'Style', 'Manaus', 'AM', '34567-890', '(92) 9999-9999', 'contato@beautystyle.com');
 
 
 
